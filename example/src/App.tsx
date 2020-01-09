61681da4786deea43d93a79b8982c73d568fd88d
Diego Laucsen da Rosa <diego.laucsen@Benes-MacBook-Pro.local>
2020-01-09 15:02:33 +0100

    feat: adding JSON structure for mage sheets

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
-import { Container, Row, Column } from './reactComponentLib';
+import RCL, { Container, Row, Column } from './reactComponentLib';
 
 const SimpleText = styled.div`
   color: blue;
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Container>
-          <Row>
-            <Column mobile={6} tablet={12} desktop={5}>
-              <SimpleText>mobile 6 / tablet 12 / desktop 5</SimpleText>
-            </Column>
-            <Column mobile={6} tablet={6} desktop={5}>
-              <SimpleText>mobile 6 / tablet 6 / desktop 5</SimpleText>
-            </Column>
-            <Column mobile={12} tablet={6} desktop={2}>
-              <SimpleText>mobile 12 / tablet 6 / desktop 2</SimpleText>
-            </Column>
-          </Row>
-        </Container>
-      </div>
-    );
+const App = () => {
+  const [type] = useState('mage');
+
+  const [structure, setStructure] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getStructure = axios(`/samples/${type}-structure.json`);
+    getStructure.then(res => setStructure(res.data));
+  }, [type]);
+  useEffect(() => {
+    if (structure) {
+      const getData = axios(`/samples/${type}-data.json`);
+      getData.then(res => setData(res.data));
+    }
+  }, [type, structure]);
+
+  if (!structure || !data) {
+    return null;
   }
-}
+
+  console.log(structure);
+  console.log(data);
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Column mobile={6} tablet={12} desktop={5}>
+            <SimpleText>mobile 6 / tablet 12 / desktop 5</SimpleText>
+          </Column>
+          <Column mobile={6} tablet={6} desktop={5}>
+            <SimpleText>mobile 6 / tablet 6 / desktop 5</SimpleText>
+          </Column>
+          <Column mobile={12} tablet={6} desktop={2}>
+            <SimpleText>mobile 12 / tablet 6 / desktop 2</SimpleText>
+          </Column>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
